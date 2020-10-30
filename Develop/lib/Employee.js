8a8304012615731ff803ed1e3d75e2efac2efc1c
sp <sam.pack.sp@gmail.com>
2020-10-30 19:06:55 +1030

    initial commit and employee clas and sub-classes

---
+++ Develop/lib/Employee.js
@@ -0,0 +1,9 @@
+// TODO: Write code to define and export the Employee class
+
+class Employee {
+    constructor(id, name, email){
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    }
+}
---
+++ Develop/lib/Manager.js
@@ -0,0 +1,9 @@
+// TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+
+class Manager extends Employee {
+    constructor(id, office){
+        super(id, name, email);
+        this.office = office;
+    }
+}
